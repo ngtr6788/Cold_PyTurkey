1103f324569a027cc8be6377ac9e27e95f960f2d
Nguyen Tran <nguyentran1072e@gmail.com>
2021-08-21 19:02:51 -0400

    Renamed trying_testing to sched_test.py

--- sched_test.py
+++ sched_test.py
@@ -6,10 +6,9 @@ from cold_pyturkey import FROZEN_TURKEY
 def main():
     day_sched = [[FROZEN_TURKEY, "00:00", "01:30"],
                  [FROZEN_TURKEY, "22:30", "23:59:59"],
-                 [FROZEN_TURKEY, "16:00", "16:30"],
-                 ["Netflix", "17:00", "17:30"]]
+                 [FROZEN_TURKEY, "17:00", "19:00"]]
 
-    print(pyturkey.schedule_blocks(day_sched))
+    pyturkey.schedule_blocks(day_sched))
 
 if __name__ == "__main__":
     main()
