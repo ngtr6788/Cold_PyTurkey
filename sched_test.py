e7e7d5e0cad1f07dc42aee81f8db71624a595e5c
Nguyen Tran <nguyentran1072e@gmail.com>
2021-08-21 22:24:02 -0400

    Modified a new Frozen Turkey schedule on sched_test

--- sched_test.py
+++ sched_test.py
@@ -5,10 +5,11 @@ from cold_pyturkey import FROZEN_TURKEY
 
 def main():
     day_sched = [[FROZEN_TURKEY, "00:00", "01:30"],
-                 [FROZEN_TURKEY, "22:30", "23:59:59"],
-                 [FROZEN_TURKEY, "17:00", "19:00"]]
+                 [FROZEN_TURKEY, "13:00", "14:30"],
+                 [FROZEN_TURKEY, "19:00", "20:30"],
+                 [FROZEN_TURKEY, "22:30", "23:59:59"]]
 
-    pyturkey.schedule_blocks(day_sched))
+    pyturkey.schedule_blocks(day_sched)
 
 if __name__ == "__main__":
     main()
