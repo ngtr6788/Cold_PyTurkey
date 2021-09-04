409759b51c3a8936b330ca224db7e8fba33f0aec
Nguyen Tran <nguyentran1072e@gmail.com>
2021-09-04 18:05:11 -0400

    Editied function names and changed implementation

--- block_chrome.py
+++ block_chrome.py
@@ -2,35 +2,37 @@ import datetime, time
 import subprocess
 from typing import Sequence, Union
 
+"""This program *TRIES* to implement blocking applications. Crossing my fingers."""
+
 now = datetime.datetime.now
 
-def block_chrome():
-    rn = now()
-    two_min = datetime.timedelta(minutes = 2)
-    while now() < (rn + two_min):
-        subprocess.Popen("taskkill /im chrome.exe /t /f", stdout = None, stderr = None, shell = False)
-        time.sleep(1)
+def taskkill(task: Union[int, str]) -> None:
+    """This kills a task either by its name (str) or its ID (int). 
+    Raises TypeError if task is neither str or int"""
+    if isinstance(task, int):
+        flag = "/pid"
+    elif isinstance(task, str):
+        flag = "/im"
+    else:
+        raise TypeError("task is not either an integer (ID number) or str (task name)")
+    subprocess.Popen(f"taskkill {flag} {task} /t /f", shell = True)
 
-def taskkill(task: Union[int, str], sec_duration = 0):
+"""
+def timed_kill_task(task: Union[int, str], sec_duration = 0):
+    # Call taskkill on a certain task for given amount of seconds
     rn = now()
     delta_duration = datetime.timedelta(seconds = sec_duration)
     while now() < (rn + delta_duration):
-        if isinstance(task, int):
-            subprocess.Popen(f"taskkill /pid {task} /t /f", shell = True)
-        elif isinstance(task, str):
-            subprocess.Popen(f"taskkill /im {task} /t /f", shell = True)
-        else:
-            raise TypeError("task is not either an integer (ID number) or str (task name)")
+        taskkill(task)
         time.sleep(0.5)
+"""
         
-def taskkill_list(task_list: Sequence[str], sec_duration = 0):
+def timed_kill_tasklist(*args: Sequence[Union[int, str]], sec_duration = 0):
+    """Calls taskkill on a list of tasks for a given amount of seconds"""
     rn = now()
     delta_duration = datetime.timedelta(seconds=sec_duration)
     while now() < (rn + delta_duration):
-        for task in task_list:
-            subprocess.Popen(f"taskkill /im {task} /t /f")
+        for task in args:
+            taskkill(task)
         time.sleep(0.5)
 
-if __name__ == "__main__":
-    pass
-    # taskkill_list(["chrome.exe", "Notion.exe", "notepad.exe", "msedge.exe"], sec_duration=30)
