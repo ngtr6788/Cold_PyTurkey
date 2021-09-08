0559335cbf8703dec8f1a7786221ea6dcd30c019
Nguyen Tran <nguyentran1072e@gmail.com>
2021-09-07 21:36:14 -0400

    Gave start function two optional options
    The start function now has two options: for and until
    for means that you block "for" a given amount of minutes
    until means that you block "until" a certain date and time
    
    This is my first implementation. Things could get weird.

--- pyturkeycli.py
+++ pyturkeycli.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import cold_pyturkey as pyturkey
 
 _FROZEN = "frozen"
@@ -8,20 +9,48 @@ def main():
 
     # I made the top-level parser for the function type only.
     parser = argparse.ArgumentParser()
-    func_parser = parser.add_subparsers(title="function")
+    func_parser = parser.add_subparsers(prog="[pyturkey | python pyturkeycli.py]", title="function")
 
     # subparser for start command
-    def parse_start(args):
-        pyturkey.start_block(args.blockname, args.minutes, args.lock)
+    """
+    ROUGH DRAFT OF HOW start WOULD WORK.
+    you could either call
+    start "Block Name"
+    start "Block Name" minutes (number) (-l)
+    start "Block Name" until (endtime) (enddate) --at (startdatetime)
+    if (date) is not mentioned, it's assumed to be today.
+    """
     pstart = func_parser.add_parser("start")
     pstart.add_argument("blockname", type=str)
-    pstart.add_argument("-l", "--lock", action="store_true")
-    pstart.add_argument("minutes", type=int)
-    pstart.set_defaults(func=parse_start)
+    start_options = pstart.add_subparsers(title="options")
+    
+    # subparser for start for
+    def parse_start(args):
+        pyturkey.start_block(args.blockname, args.minutes, args.lock)
+    pminutes = start_options.add_parser("for")
+    pminutes.add_argument("minutes", type=int, default=0)
+    pminutes.add_argument("-l", "--lock", action="store_true")
+    pminutes.set_defaults(func=parse_start)
+
+    # subparser for start until
+    def parse_start_until(args):
+        endtimeobj = datetime.time.fromisoformat(args.endtime)
+        enddateobj = pyturkey.today() if (args.enddate is None) else datetime.date.fromisocalendar(args.enddate)
+        enddatetime = datetime.datetime.combine(enddateobj, endtimeobj)
+        startdatetime = pyturkey.now() if (args.startat is None) else datetime.datetime.fromisoformat(args.startat)
+
+        pyturkey.start_block_until(args.blockname, enddatetime, startdatetime)
+
+    puntil = start_options.add_parser("until")   
+    puntil.add_argument("endtime", type=str)
+    puntil.add_argument("--enddate", type=str, default=None)
+    puntil.add_argument("--startat", type=str, required=False, default=None)
+    puntil.set_defaults(func=parse_start_until)
 
     # subparser for stop command
     def parse_stop(args):
         pyturkey.stop_block(args.blockname)
+
     ptoggle = func_parser.add_parser("stop")
     ptoggle.add_argument("blockname", type=str)
     ptoggle.set_defaults(func=parse_stop)
@@ -29,6 +58,7 @@ def main():
     # subparser for toggle command
     def parse_toggle(args):
         pyturkey.toggle_block(args.blockname)
+
     ptoggle = func_parser.add_parser("toggle")
     ptoggle.add_argument("blockname", type=str)
     ptoggle.set_defaults(func=parse_toggle)
@@ -36,6 +66,7 @@ def main():
     # subparser for add command
     def parse_add(args):
         pyturkey.add_url(args.blockname, args.url, args.exception)
+
     padd = func_parser.add_parser("add")
     padd.add_argument("blockname", type=str)
     padd.add_argument("url", type=str)
@@ -50,10 +81,10 @@ def main():
     pmdr.add_argument("blockname", type=str)
     pmdr.add_argument("blockmin", type=int)
     pmdr.add_argument("breakmin", type=int)
-    pmdr.add_argument("loops", type=int)
+    pmdr.add_argument("loops", type=int, default=1)
     pmdr.add_argument("-l", "--lock", action='store_true')
     pmdr.add_argument("--breakf", action='store_false')
-    pmdr.set_defaults(func=parse_pomodoro, loops=1)
+    pmdr.set_defaults(func=parse_pomodoro)
 
     # subparser for frozen (start frozen) command
 
